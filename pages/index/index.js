b16d703a85750eee503439e47814d56cd5ab15fd
zxk0nly1 <1272667023@qq.com>
2019-11-22 00:02:20 +0800

    首页轮播图

---
+++ pages/index/index.js
@@ -0,0 +1,10 @@
+const app = getApp()
+Page({
+  data: {
+    background: ['/images/home/01banner1.jpg', '/images/home/01banner2.jpg'],
+    indicatorDots: true,
+    autoplay: false,
+    interval: 2000,
+    duration: 500
+  }
+})
